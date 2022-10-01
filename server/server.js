3c86b8513bf747cd1f898a757079d4fa55adbd14
ashachakre0906 <chourpagar.asha@gmail.com>
2022-09-30 22:17:52 -0700

    created schema folder which includes typedefs and resolvers and also updated server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
+const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 const routes = require("./routes");
+const { authMiddleware } = require("./utils/auth");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: true }));
@@ -30,3 +33,4 @@ const startApolloServer = async (typeDefs, resolvers) => {
     );
   });
 };
+startApolloServer(typeDefs,resolvers);
