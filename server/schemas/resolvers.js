3a872fe10d5905019733cdca9b020da9aff779d4
ashachakre0906 <chourpagar.asha@gmail.com>
2022-10-03 13:44:09 -0700

    added login and adduser in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,29 @@
 const AuthenticationError = require("apollo-server-express");
-const { resolvers } = require(".");
 const { User, Book } = require("../models");
 const { signToken } = require("../utils/auth");
 
-
+const resolvers = {
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      //First we create user
+      const user = await User.create({ username, email, password });
+      //To reduce the friction for the user, we immediately sign a JSON Web Token and log in after they are created
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Icorrect credentials");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
 
 module.exports = resolvers;
