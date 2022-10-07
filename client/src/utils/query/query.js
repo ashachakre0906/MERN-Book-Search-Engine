fc6beac06647ede1e9a9aac383d499e6e3228f8a
ashachakre0906 <chourpagar.asha@gmail.com>
2022-10-06 22:59:07 -0700

    added query and mutations folder in client side plus added queries and mutations to the respective folder

---
+++ client/src/utils/query/query.js
@@ -0,0 +1,9 @@
+//This will hold the query GET_ME, which will execute the me query set up using Apollo Server.
+import { gql } from "@apollo/client";
+export const GET_ME = gql`
+query Query {
+    me {
+      _id
+    }
+  }
+`;
